d5480c2861dc4fb238b5f2c82ed7ed2946194dd7
Maxich PC <mselickij45@gmail.com>
2026-06-17 23:38:34 +0300

    Interface for saving project done

---
+++ TacticalGameEngine/src/core/interfaces/IProjectRepository.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../models/project.h"
+namespace Engine::Core::Interfaces {
+	class IProjectRepository {
+	public:
+		~IProjectRepository() = default;
+		virtual void save(const Models::Project& project, const std::string& filepath) = 0;//можно и unique использовать, но тогда у сохранения будет возможность изменять данные
+		virtual std::unique_ptr<Models::Project> load(const std::string& filepath) = 0;
+	};
+}
