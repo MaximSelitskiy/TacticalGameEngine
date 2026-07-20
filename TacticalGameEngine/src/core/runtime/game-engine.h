3f0ce672cc40e466bed6f7c9d6f79c1f42a36ad9
Maxich <mselickij45@gmail.com>
2026-07-21 00:02:20 +0300

    refactor(runtime): implement Rule of Five for Game Engine class

--- TacticalGameEngine/src/core/runtime/game-engine.h
+++ TacticalGameEngine/src/core/runtime/game-engine.h
@@ -5,24 +5,35 @@
 #include <vector>
 #include <memory>
 
-namespace Engine::Core::Runtime {
-	class GameEngine {
+namespace Engine::Core::Runtime
+{
+	class GameEngine
+	{
 	private:
 		std::unique_ptr<Models::Project> active_project_;
 		std::shared_ptr<Interfaces::ILogger> logger_;
 		std::shared_ptr<Interfaces::IGamePresenter> presenter_;
 		bool is_running_ = false;
+
 	public:
 		GameEngine(std::unique_ptr<Models::Project> project,
-			std::shared_ptr<Interfaces::ILogger> logger,
-			std::shared_ptr<Interfaces::IGamePresenter> presenter);
+				   std::shared_ptr<Interfaces::ILogger> logger,
+				   std::shared_ptr<Interfaces::IGamePresenter> presenter);
 		~GameEngine() = default;
-		//был virtual, но надо убрать, т к это не интерфейс, а просто реализация методов в cpp
-		 void start();
-		 void update();//float delta_time
-		 void end();
+
+		GameEngine(const GameEngine &) = delete;
+
+		GameEngine &operator=(const GameEngine &) = delete;
+
+		GameEngine(GameEngine &&) noexcept = default;
+
+		GameEngine &operator=(GameEngine &&) noexcept = default;
+		// был virtual, но надо убрать, т к это не интерфейс, а просто реализация методов в cpp
+		void start();
+		void update(); // float delta_time
+		void end();
 		//
-		 bool isActive();
-		 std::unique_ptr<Core::Models::Project> releaseProject();
+		bool isActive();
+		std::unique_ptr<Core::Models::Project> releaseProject();
 	};
 }
