38149ace6ea8d34fce137fcb5e1b24fe97f9df10
Maxich <mselickij45@gmail.com>
2026-07-22 20:26:30 +0300

    refactor(editor): added UI screen for menu

---
+++ TacticalGameEngine/build/CMakeFiles/TacticalGameEngine.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/TacticalGameEngine.cpp" "CMakeFiles/TacticalGameEngine.dir/TacticalGameEngine.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/TacticalGameEngine.cpp.o.d"
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/src/adapters/editor/menu-components.cpp" "CMakeFiles/TacticalGameEngine.dir/src/adapters/editor/menu-components.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/src/adapters/editor/menu-components.cpp.o.d"
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/src/adapters/runtime_ui/game-state-presenter.cpp" "CMakeFiles/TacticalGameEngine.dir/src/adapters/runtime_ui/game-state-presenter.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/src/adapters/runtime_ui/game-state-presenter.cpp.o.d"
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/src/adapters/runtime_ui/game-terminal-presenter.cpp" "CMakeFiles/TacticalGameEngine.dir/src/adapters/runtime_ui/game-terminal-presenter.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/src/adapters/runtime_ui/game-terminal-presenter.cpp.o.d"
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/src/adapters/shared/state-manager.cpp" "CMakeFiles/TacticalGameEngine.dir/src/adapters/shared/state-manager.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/src/adapters/shared/state-manager.cpp.o.d"
+  "/home/maxich-prime/Programming/Pet-Projects/TacticalGameEngine/TacticalGameEngine/src/infrastcructure/persistance/json_project_repository.cpp" "CMakeFiles/TacticalGameEngine.dir/src/infrastcructure/persistance/json_project_repository.cpp.o" "gcc" "CMakeFiles/TacticalGameEngine.dir/src/infrastcructure/persistance/json_project_repository.cpp.o.d"
+  "" "TacticalGameEngine" "gcc" "CMakeFiles/TacticalGameEngine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TacticalGameEngine/build/src/core/CMakeFiles/engine_core.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine_core.dir/models/unit.cpp.o"
+  "CMakeFiles/engine_core.dir/models/unit.cpp.o.d"
+  "CMakeFiles/engine_core.dir/runtime/game-engine.cpp.o"
+  "CMakeFiles/engine_core.dir/runtime/game-engine.cpp.o.d"
+  "CMakeFiles/engine_core.dir/utils/id-generator.cpp.o"
+  "CMakeFiles/engine_core.dir/utils/id-generator.cpp.o.d"
+  "libengine_core.a"
+  "libengine_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
